0d89f6c6bff8383122785df8c1406b263f59375f
Raull2015 <raullfire@gmail.com>
2017-10-10 17:43:55 -0600

    commit inicial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drenajesWebInterfazApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the drenajesWebInterfazApp
+ */
+angular.module('drenajesWebInterfazApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name drenajesWebInterfazApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the drenajesWebInterfazApp
+ */
+angular.module('drenajesWebInterfazApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
