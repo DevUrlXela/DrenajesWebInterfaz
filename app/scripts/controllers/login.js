c6586cc96bcd7955c9ef19a1c451eba04a8b2d1d
Raull2015 <raullfire@gmail.com>
2017-10-18 16:44:48 -0600

    firebase

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -7,10 +7,10 @@ angular.module('drenajesWebInterfazApp')
     $scope.password = null
     $scope.logearse = function(){
 
-      //apiService.crear('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDMWY9z1PPeX-6adeIMRZBwk87ilFeH3bw',{"email" : $scope.user ,"password" : $scope.password,"returnSecureToken": true})
-    apiService.crear('http://localhost:8000/usuarios/user/login/',{"username" : $scope.user ,"password" : $scope.password})
+    apiService.crear('https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDMWY9z1PPeX-6adeIMRZBwk87ilFeH3bw',{"email" : $scope.user ,"password" : $scope.password,"returnSecureToken": true})
+    //apiService.crear('http://localhost:8000/usuarios/user/login/',{"username" : $scope.user ,"password" : $scope.password})
     .then(function successCallback(response) {
-      sesion.login(response.data.key, response.data.user, response.data.user)
+      sesion.login(response.data.idToken, response.data.email, response.data.email)
       $location.url('/')
     }, function errorCallback(response) {
       $scope.respuesta = 'Usuario o contraseña incorrectos';
