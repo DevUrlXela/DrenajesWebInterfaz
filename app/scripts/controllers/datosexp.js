ffe7af41e2d2871156c5c9eb90aa2a0079249705
Raull2015 <raullfire@gmail.com>
2017-11-10 11:25:38 -0600

    cambios en la interfaz

--- app/scripts/controllers/datosexp.js
+++ app/scripts/controllers/datosexp.js
@@ -1,4 +1,21 @@
 angular.module('drenajesWebInterfazApp')
   .controller('DetalleCtrl', function ($scope, $location, $routeParams, apiService, sesion) {
-      console.log($routeParams.id)
+
+    apiService.post('/expedientes/expediente/' + $routeParams.id + '/leido/', { "token": sesion.getToken() }, sesion.getToken())
+    .then(function successCallback(response) {
+      console.log('funciona')
+    }, function errorCallback(response) {
+      console.log('error')
+    })
+
+    apiService.get( '/expedientes/expediente/informacion/' + $routeParams.id + '/', sesion.getToken())
+    .then(function successCallback(response) {
+      console.log(response.data)
+    }, function errorCallback(response) {
+
+    })
+
+
+
+
   });
