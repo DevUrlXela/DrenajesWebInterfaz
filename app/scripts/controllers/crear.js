3084a01edab2ee68c1427ceef4a5a06914e006f6
Raull2015 <raullfire@gmail.com>
2017-11-11 14:31:08 -0600

    detalle expediente

--- app/scripts/controllers/crear.js
+++ app/scripts/controllers/crear.js
@@ -1,10 +1,10 @@
 angular.module('drenajesWebInterfazApp')
   .controller('CrearExpCtrl', function ($scope,apiService,sesion) {
-      
+      d = new Date()
       $scope.expediente = {
         id : '',
         folio : '',
-        fecha : new Date(),
+        fecha : d.getDate() + '/' + (d.getMonth()+1) + '/' + d.getFullYear(),
         tipo : '',
         remitente : '',
         firma : ''
@@ -14,8 +14,22 @@ angular.module('drenajesWebInterfazApp')
       $scope.cumple = true
       $scope.agregar_req = function(){
         if($scope.descripcion!=''){
-          $scope.requisitos.push({ descripcion : $scope.descripcion, cumple:$scope.cumple, necesario:true})
+          $scope.requisitos.push({ descripcion : $scope.descripcion, cumple:$scope.cumple})
           console.log($scope.requisitos)
         }
+      }
+
+      $scope.crear_exp = function(){
+          if($scope.expediente.id == '' || $scope.expediente.folio == '' || $scope.fecha == '' || $scope.expediente.tipo == '' || $scope.remitente == '' || $scope.expediente.firma == ''){
+            $scope.alerta = 'No puede dejar campos vacios'
+            return null
+          }
+
+        apiService.post('/expedientes/expediente/nuevo', )
+        .then(function successCallback(response) {
+            
+        }, function errorCallback(response) {
+
+        })
       }
   });
--- app/scripts/controllers/transferir.js
+++ app/scripts/controllers/transferir.js
@@ -1,4 +1,13 @@
 angular.module('drenajesWebInterfazApp')
-  .controller('TransCtrl', function ($scope, $location,apiService,sesion) {
-    
+  .controller('TransCtrl', function ($scope, $routeParams, $location,apiService,sesion) {
+    $scope.id_exp = $routeParams.id
+    $scope.razon = 'nada'
+    $scope.observacion = 'nada'
+    $scope.usuarios = []
+    //obtener usuarios
+
+    $scope.enviar = function(){
+
+    }
+
   });
