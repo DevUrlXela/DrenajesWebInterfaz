2dfdddf0bff36e46e29e229e955446392366cda9
Raull2015 <raullfire@gmail.com>
2017-11-17 08:59:37 -0600

    reportes preparacion

--- app/scripts/controllers/transferir.js
+++ app/scripts/controllers/transferir.js
@@ -2,19 +2,28 @@ angular.module('drenajesWebInterfazApp')
   .controller('TransCtrl', function ($scope, $routeParams, $location,apiService,sesion) {
     $scope.id_exp = $routeParams.id
     $scope.razon = ''
-    $scope.observacion = ''
-    $scope.usuarios = []
-    $scope.enviado = 0
+    $scope.observacion = 'abcdddd'
+    $scope.enviado = '0'
 
     apiService.get('/expedientes/rol/usuarios/', sesion.getToken())
     .then(function successCallback(response){
-      $scope.usuarios = response.data
+       user_temp = response.data
+       for(i = 0; i<user_temp.length;i=+1){
+         if(user_temp[i].id == sesion.getId()){
+           user_temp.splice(i,1)
+         }
+       }
+      $scope.usuarios = user_temp
     })
 
     $scope.enviar = function(){
       if($scope.razon != '' && $scope.observacion != '' && $scope.enviado != 0){
-        apiService.post('/expedientes/actualizacion/expediente/' + $routeParams.id + '/crear/', {'observaciones':$scope.observacion,'enviado':sesion.getId(),'recibido' : $scope.enviado } , sesion.getToken())
+        apiService.post('/expedientes/actualizacion/expediente/' + $routeParams.id + '/crear/', {'observaciones':$scope.razon,'enviado':sesion.getId(),'recibido' : $scope.enviado } , sesion.getToken())
         .then(function successCallback(response){
+          apiService.post( '/expedientes/observacion/expediente/' + $routeParams.id + '/crear/' , {"token":sesion.getToken(), "observacion":$scope.observacion}, sesion.getToken())
+          .then(function successCallback(response) {
+            $route.reload()
+          })
           swal({
             title: "Expediente enviado",
             type: "success"
